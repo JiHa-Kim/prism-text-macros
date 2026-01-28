48b2277e28f69eed578fe736dbd1284f7392f6c5
jiha <jihakim@outlook.com>
2026-01-28 13:38:18 -0500

    deps: add MathJax library

---
+++ public/libs/mathjax-config.js
@@ -0,0 +1,13 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true
+  },
+  options: {
+    enableMenu: false
+  },
+  startup: {
+    typeset: false
+  }
+};
