c0a0fe774ba92239f4c7848c8261f251cbd48884
jiha <jihakim@outlook.com>
2026-01-28 15:34:19 -0500

    fix: delete mathjax `noundefined` package

--- public/libs/mathjax-config.js
+++ public/libs/mathjax-config.js
@@ -3,10 +3,10 @@ window.MathJax = {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     displayMath: [['$$', '$$'], ['\\[', '\\]']],
     processEscapes: true,
-    packages: {'[+]': ['noerrors', 'noundefined', 'color', 'textmacros', 'mathtools', 'physics', 'braket']} 
+    packages: {'[+]': ['noerrors', 'color', 'textmacros', 'mathtools', 'physics', 'braket']} 
   },
   loader: {
-    load: ['[tex]/noerrors', '[tex]/noundefined', '[tex]/color', '[tex]/textmacros', '[tex]/mathtools', '[tex]/physics', '[tex]/braket']
+    load: ['[tex]/noerrors', '[tex]/color', '[tex]/textmacros', '[tex]/mathtools', '[tex]/physics', '[tex]/braket']
   },
   options: {
     enableMenu: false
